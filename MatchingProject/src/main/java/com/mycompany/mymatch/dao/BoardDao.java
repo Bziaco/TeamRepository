ce23c939b699411d40bdf35ebb326bbc83b97e30
joh2787 <joh2787@naver.com>
2016-11-18 18:13:49 +0900

    Dao, Dto 생성

--- MatchingProject/src/main/java/com/mycompany/mymatch/dao/BoardDao.java
+++ MatchingProject/src/main/java/com/mycompany/mymatch/dao/BoardDao.java
@@ -1,11 +1,50 @@
 package com.mycompany.mymatch.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import com.mycompany.mymatch.dto.Board;
+import com.mycompany.mymatch.dto.Member;
 
 @Component
 public class BoardDao {
 	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public int insert(Board board) {
+		String sql="insert into board(bno, btitle, bcontent, bwriter, bcount) values(?,?,?,?,?)";
+		int row=jdbcTemplate.update(
+				sql,
+				board.getBno(),
+				board.getBtitle(),
+				board.getBcontent(),
+				board.getBwriter(),
+				board.getBcount()
+				);
+		return row;
+	}
+
+
+	
+//---------------------------------------------------------------------------------------------------------------------------------
+
+		public int update(Board board) {
+			String sql = "update member set ";
+			
+			return 0;
+		}
+		
+//---------------------------------------------------------------------------------------------------------------------------------	
+		
+		public int delete(int bno) {
+			String sql = "delete from board where bno=?";
+			int row =jdbcTemplate.update(sql, bno);
+			return row;
+		}
+		
+//---------------------------------------------------------------------------------------------------------------------------------	
+
 	
 
 }
